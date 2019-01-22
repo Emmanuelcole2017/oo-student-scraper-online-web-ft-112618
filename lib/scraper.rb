9746c8ce5ff0c66bb91911c3361ce1b6afb2cb34
Emmanuel Cole <emmanuel.c2011@gmail.com>
2019-01-22 16:29:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -34,7 +34,8 @@ class Scraper
   # student's profile page and returns a hash of attributes
   # describing an individual student
   def self.scrape_profile_page(profile_url)
-    
+    Nokogiri::HTML(open(profile_url))
+    binding.pry
   end
   
   
