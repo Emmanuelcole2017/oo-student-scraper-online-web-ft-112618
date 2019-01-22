ecc9118f291fbbc9627469d045cb191318c3e3e0
Emmanuel Cole <emmanuel.c2011@gmail.com>
2019-01-22 15:11:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,6 +14,7 @@ class Scraper
     # returns [{:name, :location, :profile_url},
     # {:name, :location, :profile}] from Nokogiri
     # using open-URI
+    Nokogiri::HTML
   end
 
   def self.scrape_profile_page(profile_url)
