72e7cd1e1c4369b1109e31314928ea03d78fd6bb
Emmanuel Cole <emmanuel.c2011@gmail.com>
2019-01-22 18:50:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,12 +7,17 @@ class Student
   # #new takes in an argument of a hash and sets that new 
   # student's attributes using the key/value pairs of that
   # hash.
+  # adds that new student to the Student class' collection of
+  # all existing students, stored in the '@@all' class 
+  # variable.
   def initialize(student_hash)
     student_hash.each {|key, value| self.send(("#{key}="), value)
     }
     @@all << self
   end
 
+  # .create_from_collection uses the Scraper class to create
+  # new students with the correct name and location.
   def self.create_from_collection(students_array)
     
   end
